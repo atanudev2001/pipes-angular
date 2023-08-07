2dfcb4b19dd5c190be33e4c19bf07ae45947ba2c
atanu sarkar <atanu.sarkar@bosenet.com>
2023-08-07 21:15:47 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{datas}from '../app/data'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  coursedata=datas;
+
+  startdate = new Date(2023,7,3 )
+  price = 9.99;
+  rate = 0.454;
+
+  firstcourse = datas[0];
+
+
 }
---
+++ src/app/data.ts
@@ -0,0 +1,59 @@
+
+export const datas:any =[
+  {
+    id:1,
+    title:"Angular deep drive course",
+    description:"lorem",
+    image:"https://cdn.freebiesupply.com/logos/large/2x/angular-icon-1-logo-png-transparent.png",
+    category:'beginner'
+  },
+  {
+    id:2,
+    title:"React deep drive course",
+    description:"lorem Ipsum",
+    image:"https://th.bing.com/th/id/OIP.K-4RqDC6zFrpAG31ayDDOgHaHa?pid=ImgDet&rs=1s",
+    category:''
+  },
+  {
+    id:3,
+    title:"Java Crash course",
+    description:"Zero to expert",
+    // image:"https://wallpapercave.com/wp/wp6599912.jpg",
+    category:'beginner'
+  },
+  {
+    id:4,
+    title:"Spring Boot Basic to advance",
+    description:"All information of Spring Boot",
+    image:"https://th.bing.com/th/id/OIP.iG4Qt0D9g9yLNxWZmUN8NgHaD4?pid=ImgDet&rs=1",
+    category:'beginner'
+  },
+  {
+    id:5,
+    title:"Blockchain-Mastery",
+    description:"Basic to Advance in 10 days",
+    // image:"https://cdn5.vectorstock.com/i/1000x1000/65/44/logo-blockchain-vector-20896544.jpg",
+    category:'intermediate'
+  },
+  {
+    id:6,
+    title:"Web Development- Basic to Advanced",
+    description:"Basic to Advance in 10 days",
+    image:"https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_1080,q_100,w_1080/v1/gcs/platform-data-dsc/events/web_ilY3NoA.jpeg",
+    category:'advanced'
+  },
+  {
+    id:7,
+    title:"Robotics Crash Course",
+    description:"No previous knowledge is required",
+    image:"https://static.wixstatic.com/media/c20227_6bf000ee3dc3431f93e948a626190048~mv2.jpg/v1/fill/w_980,h_558,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c20227_6bf000ee3dc3431f93e948a626190048~mv2.jpg",
+    category:'intermediate'
+  },
+  {
+    id:8,
+    title:"UI/UX using Figma",
+    description:"",
+    // image:"https://th.bing.com/th/id/OIP.XZEUiIrDsNt8EXBteZucSQHaEv?pid=ImgDet&rs=1",
+    category:'advanced'
+  }
+]
